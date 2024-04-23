ccfa5c6ec1d6417e0e38153a9e878b9379cc9566
Luãn F <luanfernandes.ch004@academico.ifsul.edu.br>
2024-04-23 00:11:04 -0300

    SQL connections with MySQL functions

---
+++ routes/livros.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const { body, validationResult } = require('express-validator'); 
+const router = express.Router();
+const db = require('../src/mysql');
+
+router
+.route("/:book")
+.get(async (req, res) => {
+    res.send('Get books list by ID ')
+
+    const bookID = parseInt(req.params.book);
+
+    try {
+        console.log(`Querying for bookID ${bookID}...`);
+        const book = await db.getBookByID(bookID);
+        res.json(book);
+    } catch (err) {
+        console.error(err);
+    }
+    })
+
+.put((req, res) => {
+    res.send(`Atualizar livro com ID ${req.params.book}`)
+})
+
+.delete(async(req, res) => {
+    res.send(`Deletar livro com ID ${req.params.book}`)
+
+    const bookID = parseInt(req.params.book)
+    try {
+        console.log(`Purging book ${bookID}...`);
+        const purge = await db.deleteRow(bookID);
+        res.send(`Book ${bookID} has been purged.`)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router
+.get('/', async(req, res, next) => {
+    
+    try {
+        const userLibrary = await db.queryBooks();
+        res.send('Book query has been sent')
+
+    } catch(err){
+        console.log(err);
+    }
+});
+
+router
+.post('/form',
+    [
+    body('book').notEmpty().isString(), 
+    body('pags').notEmpty().isInt(), 
+    ], 
+    (req, res) =>{
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        console.log(`
+        Field: ${errors.param}, 
+        Value: ${req.body[errors.param]}, 
+        Error: ${errors.msg}`);
+        
+        return res.status(400).json({ errors: errors.array() })
+    }
+    
+    try{
+        const newBook = db.createBookRow(req.body.book, req.body.pags);
+        console.log(req.body);
+
+        res.sendStatus(201);
+    } catch (err){
+        console.log(err);
+    }
+});
+
+module.exports = router
--- src/mysql.js
+++ src/mysql.js
@@ -1,16 +1,84 @@
-/*
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'admin',
-    user: 'konataware',
-    password: 'admin123',
+const { query } = require('express');
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Admin123$',
+    port: '3306',
     database: 'library1'
-});
+})
+console.log('BD conectado!')
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL server');
-});
+module.exports = {
+    pool,
+    queryBooks,
+    getBookByID,
+    createBookRow,
+    deleteRow,
 
-module.exports = connection;
-*/
+}
+
+async function queryBooks(){
+    try{
+        const [rows] = await pool.query('SELECT * FROM Biblioteca');
+
+        // Access the values
+        for (let row of rows) {
+        console.log({
+            id: row.LivroID,
+            book: row.LivroTitulo,
+            pags: row.LivroPaginas
+            });  
+        };
+    } catch(err) {
+        console.log(`Oh não! Erro encontrado: ${err}`);
+    }
+}
+
+async function getBookByID(inputID){
+    try{
+        const [rows] = await pool.query(`SELECT LivroTitulo, LivroPaginas FROM Biblioteca WHERE LivroID = ${inputID}`);
+        if(rows.length > 0){
+            console.log({
+                id: inputID,
+                book: rows[0].LivroTitulo,
+                pags: rows[0].LivroPaginas,
+                });
+        }
+        else{
+            console.log(`Erro! Nenhum livro encontrado com o ID: ${inputID}`)
+        }
+
+
+
+    } catch(err){
+        console.log(`Oh não! Erro encontrado: ${err}`);
+    }
+}
+
+async function createBookRow(title, pags){
+    try{
+        const [rows] = await pool.query(
+            `INSERT INTO Biblioteca(LivroTitulo, LivroPaginas)
+            VALUES(?, ?)`,
+            [title, pags]
+        );
+        console.log(`Book ${title} has been created.`)
+        
+    } catch(err){
+        console.log(`Oh não! Erro encontrado: ${err}`);
+    }
+}
+
+async function deleteRow(inputID){
+    try{
+        const [rows, fields] = await pool.query(`DELETE FROM Biblioteca WHERE LivroID = ${inputID}`)
+        console.log(`Book from ${inputID}, has been deleted.`)
+
+
+    } catch(err){
+        console.log(`Oh não! Erro encontrado: ${err}`);
+    }
+}
+
